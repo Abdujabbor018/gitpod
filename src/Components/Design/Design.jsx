4e9d695650c8e575654422034e8c0cd7b292bb75
Abdujabbor <mahamatsiddiqovandujabbor@gmail.com>
2026-03-16 10:29:26 +0500

    update

--- src/Components/Design/Design.jsx
+++ src/Components/Design/Design.jsx
@@ -11,7 +11,7 @@ const Design = () => {
               Remote-first. Secure by design.
             </h1>
             <img src={Img} alt="" className="design-img" />
-            <p className="design info">
+            <p className="design-info">
               You no longer need an over-powered laptop to code, Gitpod works just as <br />
               smoothly on a Chromebook or iPad. All you need is a browser. Gitpod centralizes  <br />
               all source code and never stores it on insecure machines and networks.
