dd01a1c4c7c76e51f3d83ca558a28b7bf6fbc5b3
Abdujabbor <mahamatsiddiqovandujabbor@gmail.com>
2026-03-16 10:26:54 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/Header'
 import Section from './Components/Section/Section'
+import Design from './Components/Design/Design'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
     <Navbar />
     <Header />
     <Section />
+    <Design />
     </>
   )
 }
--- src/Components/Design/Design.jsx
+++ src/Components/Design/Design.jsx
@@ -4,21 +4,21 @@ import './Design.css'
 const Design = () => {
   return (
     <>
-    <div className="design">
+      <div className="design">
         <div className="container">
-            <div className="design-container">
-                <h1 className="design-text">
-                  Remote-first. Secure by design.
-                </h1>
-                <img src={Img} alt="" className="design-img" />
-                <p className="design info">
-                  You no longer need an over-powered laptop to code, Gitpod works just as <br /> 
-                  smoothly on a Chromebook or iPad. All you need is a browser. Gitpod centralizes  <br />
-                  all source code and never stores it on insecure machines and networks.
-                </p>
-            </div>
+          <div className="design-container">
+            <h1 className="design-text">
+              Remote-first. Secure by design.
+            </h1>
+            <img src={Img} alt="" className="design-img" />
+            <p className="design info">
+              You no longer need an over-powered laptop to code, Gitpod works just as <br />
+              smoothly on a Chromebook or iPad. All you need is a browser. Gitpod centralizes  <br />
+              all source code and never stores it on insecure machines and networks.
+            </p>
+          </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
